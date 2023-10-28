b59b35cccd1456cb8e601c4d38b14d6bae8fe175
Abdul Rahman <102407007+AbdulRahmanGit@users.noreply.github.com>
2023-10-28 23:42:49 +0530

    Update server.js(temp)

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ app.use(express.json())
 app.use(cors())
 
 const API_KEY = process.env.API_KEY
-app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
+
 app.post('/completions', async (req,res) =>{
     const options = {
         method: "POST",
